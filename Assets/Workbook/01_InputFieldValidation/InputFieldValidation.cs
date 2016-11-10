ceddef7ff9ede5a8404f97d6f23ae4ed60740c4b
酒井 峰志 <sakai@mobcast.jp>
2016-11-10 16:20:34 +0900

    課題1(InputFieldValidation)を追加

---
+++ Assets/Workbook/01_InputFieldValidation/InputFieldValidation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class InputFieldValidation : MonoBehaviour
+{
+	[SerializeField]InputField inputField;
+
+	[SerializeField]Button button;
+
+	[SerializeField]Text text;
+
+	void Start()
+	{
+		//...
+	}
+}
