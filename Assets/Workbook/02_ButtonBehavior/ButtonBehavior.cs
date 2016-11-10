05f04a3d561bc7226b7439aeea1e67002826cbcc
酒井 峰志 <sakai@mobcast.jp>
2016-11-10 16:50:19 +0900

    課題2(ButtonBehavior)を追加

---
+++ Assets/Workbook/02_ButtonBehavior/ButtonBehavior.cs
@@ -0,0 +1,44 @@
+﻿/*
+
+課題2: ButtonBehavior
+
+https://mobcast.atlassian.net/wiki/pages/viewpage.action?pageId=75432125
+
+ボタンA〜Dを、以下のように実装しなさい。
+
+* ボタンA    2回目まではクリックしてもなにも起こらない。
+			3回目以降は、クリックされたらTextに「ボタンA起動」と表示する。
+
+* ボタンB  	4回目にクリックしたときだけ、Textに「ボタンB起動」と表示する。
+			5回目以降はクリックしてもなにも起こらない。
+
+* ボタンC	    2秒間押し続けたとき、Textに「ボタンC起動」と表示する。
+
+* ボタンD	    ボタンA,B,Cを全て起動したときだけ、ボタンが有効化される。
+			ボタンをクリックしたとき、Textに「ボタンD起動」と表示する。
+
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UniRx;
+
+public class ButtonBehavior : MonoBehaviour {
+
+	[SerializeField]Button buttonA;
+
+	[SerializeField]Button buttonB;
+
+	[SerializeField]Button buttonC;
+
+	[SerializeField]Button buttonD;
+
+	[SerializeField]Text text;
+
+
+	// Use this for initialization
+	void Start () {
+		//...
+	}
+}
