6f7fa9d395a45a69884fedf9715ec170cdec50f4
酒井 峰志 <sakai@mobcast.jp>
2016-11-14 11:01:16 +0900

    課題3(ClickCountUp)を追加

---
+++ Assets/Workbook/03_ClickCountUp/ScoreModel.cs
@@ -0,0 +1,33 @@
+﻿/*
+【数値が変更された時にコールバックを実行する】
+ボタンを押したらスコアが加算される機能を実装しなさい。
+
+・UIにはゲームスタートボタンと得点テキストが表示されている
+・ボタンを押すと、得点が+1される
+・得点が更新されると、得点テキストが更新される
+
+なお、この機能は「ScoreModel」「ScoreView」「ClickCountUp」の3つのクラスを利用する。
+・ScoreModel: スコアを格納するクラス
+・ScoreView: スコアを表示するクラス
+・ClickCountUp: スコアを更新するクラス
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UniRx;
+
+namespace Workbook03
+{
+	/// <summary>
+	/// スコアを格納するクラス.
+	/// ※本来ならシングルトンにしたほうが良いです。
+	/// </summary>
+	public class ScoreModel : MonoBehaviour
+	{
+		/// <summary>
+		/// スコア.
+		/// </summary>
+		public IntReactiveProperty score = new IntReactiveProperty();
+	}
+}
---
+++ Assets/Workbook/03_ClickCountUp/ScoreView.cs
@@ -0,0 +1,43 @@
+﻿/*
+【数値が変更された時にコールバックを実行する】
+ボタンを押したらスコアが加算される機能を実装しなさい。
+
+・UIにはゲームスタートボタンと得点テキストが表示されている
+・ボタンを押すと、得点が+1される
+・得点が更新されると、得点テキストが更新される
+
+なお、この機能は「ScoreModel」「ScoreView」「ClickCountUp」の3つのクラスを利用する。
+・ScoreModel: スコアを格納するクラス
+・ScoreView: スコアを表示するクラス
+・ClickCountUp: スコアを更新するクラス
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UniRx;
+
+namespace Workbook03
+{
+	/// <summary>
+	/// 「ScoreModel.score」が変更されると、テキストの内容を書き換えるクラス.
+	/// </summary>
+	public class ScoreView : MonoBehaviour
+	{
+		/// <summary>
+		/// スコアモデル.
+		/// ※本来ならシングルトンにしたほうが良いです。
+		/// </summary>
+		public ScoreModel scoreModel;
+
+		/// <summary>
+		/// スコアテキスト.
+		/// </summary>
+		public Text textScore;
+
+		// Use this for initialization
+		void Start()
+		{
+		}
+	}
+}
