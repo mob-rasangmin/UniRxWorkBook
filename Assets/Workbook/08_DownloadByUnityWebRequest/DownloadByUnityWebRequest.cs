46a4424d30fd841126db758bf5c1130110bb98b9
酒井 峰志 <sakai@mobcast.jp>
2016-11-24 11:01:33 +0900

    課題8(DownloadByUnityWebRequest)追加

---
+++ Assets/Workbook/08_DownloadByUnityWebRequest/DownloadByUnityWebRequest.cs
@@ -0,0 +1,78 @@
+﻿/*
+
+Confluence : ヒント、回答等はこちら
+https://mobcast.atlassian.net/wiki/pages/viewpage.action?pageId=80445442
+
+
+【UnityWebRequestを使った通信】
+Unityのスクリプトより、以下のzipファイルをダウンロードしなさい。
+ただし、ローカルストレージに保存する必要はありません。
+https://github.com/neuecc/UniRx/archive/5.5.0.zip
+
+
+（練習問題）
+https://docs.unity3d.com/ja/current/Manual/UnityWebRequest.html
+https://docs.unity3d.com/ja/current/ScriptReference/Experimental.Networking.UnityWebRequest.html
+・初めの状態は、ボタンを押すとUnityWebRequest.Getを使ってダウンロードが実行され、完了時にコンソールに「WebRequest完了」と表示されます。確認しなさい。
+　　・★WWWと比較し、使い方の違いを把握しましょう。
+・UnityWebRequest.Getを使ってダウンロードしなさい。完了後、Textに「WebRequest完了」と表示しなさい。
+・UnityWebRequest.Getを使って、プログレスバー付きでダウンロードしなさい。
+・UnityWebRequest.Getを使って、Textに「現在のダウンロード済みサイズ」を表示しなさい。
+
+※練習問題で利用したスクリプトは、残しておきましょう。のちほど、UnityWebRequestとObservableWebRequestを比較します。
+
+
+（問題）
+・ObservableWebRequest.Getを使って、ダウンロードしなさい。完了後、Textに「ObservableWebRequest完了」と表示しなさい。
+・ObservableWebRequest.Getを使って、プログレスバー付きでダウンロードしなさい。
+・ObservableWebRequest.Getを使って、Textに「現在のダウンロード済みサイズ」を表示しなさい。
+・通信中はボタンを押せない(interactable=false)ようにしなさい。
+
+
+（注意）
+WebRequestは、通信が完了するまでレスポンスヘッダーを取得できません（WWWと同様）。
+
+*/
+
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using System;
+using System.Linq;
+
+
+public class DownloadByUnityWebRequest : MonoBehaviour
+{
+
+	[SerializeField]Button button;
+	[SerializeField]Text text;
+	[SerializeField]Slider progressBar;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		button.OnClickAsObservable()
+			.Subscribe(_ =>
+		{
+			StartCoroutine(DownloadWebRequest("https://github.com/neuecc/UniRx/archive/5.5.0.zip"));
+		});
+	}
+
+	/// <summary>
+	/// urlを指定して、完了したらコンソールに「WebRequest完了」って表示するコルーチン
+	/// </summary>
+	IEnumerator DownloadWebRequest(string url)
+	{
+		Debug.Log("WebRequest開始");
+		UnityWebRequest web = UnityWebRequest.Get(url);
+		yield return web.Send();
+
+		Debug.Log("WebRequest完了: ダウンロードサイズ" + web.downloadedBytes);
+	}
+}
---
+++ Assets/Workbook/08_DownloadByUnityWebRequest/ObservableWebRequest.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+#if UNITY_5_4_OR_NEWER
+using UnityEngine.Networking;
+
+#else
+using UnityEngine.Experimental.Networking;
+#endif
+
+#if !UniRxLibrary
+using ObservableUnity = UniRx.Observable;
+#endif
+
+namespace UniRx
+{
+	#if !(UNITY_METRO || UNITY_WP8) && (UNITY_4_4 || UNITY_4_3 || UNITY_4_2 || UNITY_4_1 || UNITY_4_0_1 || UNITY_4_0 || UNITY_3_5 || UNITY_3_4 || UNITY_3_3 || UNITY_3_2 || UNITY_3_1 || UNITY_3_0_0 || UNITY_3_0 || UNITY_2_6_1 || UNITY_2_6)
+	// Fallback for Unity versions below 4.5
+	using Hash = System.Collections.Hashtable;
+	using HashEntry = System.Collections.DictionaryEntry;    
+	
+#else
+	// Unity 4.5 release notes:
+	// WWW: deprecated 'WWW(string url, byte[] postData, Hashtable headers)',
+	// use 'public WWW(string url, byte[] postData, Dictionary<string, string> headers)' instead.
+	using Hash = System.Collections.Generic.Dictionary<string, string>;
+	using HashEntry = System.Collections.Generic.KeyValuePair<string, string>;
+	#endif
+
+	public static partial class ObservableWebRequest
+	{
+		static UnityWebRequest CreateWebRequest(string url, byte[] postData, Hash headers)
+		{
+			//Create UnityWebRequest
+			UnityWebRequest web = (postData != null)
+				? new UnityWebRequest(url, UnityWebRequest.kHttpVerbPOST, new DownloadHandlerBuffer(), new UploadHandlerRaw(postData))
+				: UnityWebRequest.Get(url);
+
+			//Set Header
+			if (headers != null)
+			{
+				foreach (var pair in headers)
+				{
+					if (!string.IsNullOrEmpty(pair.Value))
+						web.SetRequestHeader(pair.Key, pair.Value);
+				}
+			}
+			return web;
+		}
+
+		public static IObservable<UnityWebRequest> Get(string url, Hash headers = null, IProgress<float> reportProgress = null)
+		{
+			return ObservableUnity.FromCoroutine<UnityWebRequest>((observer, cancellation) => Fetch(CreateWebRequest(url, null, headers), observer, reportProgress, cancellation));
+		}
+
+		public static IObservable<UnityWebRequest> Post(string url, byte[] postData, Hash headers = null, IProgress<float> reportProgress = null)
+		{
+			return ObservableUnity.FromCoroutine<UnityWebRequest>((observer, cancellation) => Fetch(CreateWebRequest(url, postData, headers), observer, reportProgress, cancellation));
+		}
+
+
+		static IEnumerator Fetch(UnityWebRequest www, IObserver<UnityWebRequest> observer, IProgress<float> reportProgress, CancellationToken cancel)
+		{
+			www.Send();
+
+			using (www)
+			{
+				while (!www.isDone && !cancel.IsCancellationRequested)
+				{
+					if (reportProgress != null)
+					{
+						try
+						{
+							reportProgress.Report(www.downloadProgress);
+						}
+						catch (Exception ex)
+						{
+							observer.OnError(ex);
+							yield break;
+						}
+					}
+					yield return null;
+				}
+
+				if (cancel.IsCancellationRequested)
+				{
+					www.Abort();
+					yield break;
+				}
+
+				if (reportProgress != null)
+				{
+					try
+					{
+						reportProgress.Report(www.downloadProgress);
+					}
+					catch (Exception ex)
+					{
+						observer.OnError(ex);
+						yield break;
+					}
+				}
+
+				if (www.isError)
+				{
+					observer.OnError(new WebRequestErrorException(www, www.responseCode, www.error));
+				}
+				else
+				{
+					observer.OnNext(www);
+					observer.OnCompleted();
+				}
+			}
+		}
+	}
+
+	public class WebRequestErrorException : Exception
+	{
+		public string RawErrorMessage { get; private set; }
+
+		public string Text { get; private set; }
+
+		public HttpStatusCode StatusCode { get; private set; }
+
+		public Dictionary<string, string> ResponseHeaders { get; private set; }
+
+		public UnityWebRequest webRequest { get; private set; }
+
+		public WebRequestErrorException(UnityWebRequest www, long responseCode, string text)
+		{
+			this.webRequest = www;
+			this.RawErrorMessage = www.error;
+			this.ResponseHeaders = www.GetResponseHeaders();
+			this.Text = text; 
+			this.StatusCode = (HttpStatusCode)responseCode;
+		}
+
+		public override string ToString()
+		{
+			var text = this.Text;
+			if (string.IsNullOrEmpty(text))
+			{
+				return RawErrorMessage;
+			}
+			else
+			{
+				return RawErrorMessage + " " + text;
+			}
+		}
+	}
+    
+}
